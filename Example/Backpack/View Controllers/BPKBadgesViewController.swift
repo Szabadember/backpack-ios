63e46d7242ca0a0d86f72f435932e2b382d20405
George Gillams <george.gillams@skyscanner.net>
2018-11-06 16:56:58 +0000

    More WIP

---
+++ Example/Backpack/View Controllers/BPKBadgesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BPKBadgesViewController.swift
+//  Backpack Native
+//
+//  Created by George Gillams on 06/11/2018.
+//  Copyright © 2018 Skyscanner. All rights reserved.
+//
+
+import Foundation
--- Example/Backpack/Views/BadgesView.swift
+++ Example/Backpack/Views/BadgesView.swift
@@ -11,22 +11,23 @@ import UIKit
 class BadgesView: UIView {
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var typeLabel: UILabel!
-    
+
     // TODO add type property, which will be used to set Label text and also set badge types
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
     }
 
-    override required init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
     }
 
     private func commonInit() {
+        print("HERE")
         Bundle.main.loadNibNamed("BadgesView", owner: self, options: nil)
-        addSubview(contentView)
+        self.addSubview(self.contentView)
         contentView.frame = self.bounds
         contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
     }
